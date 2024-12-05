078871c6bc3bb265dce218ee48ec5523bd2c6832
chamumaramba <trustmaramba@gmail.com>
2024-12-05 22:02:49 +0100

    Apply stashed changes for 'Hello ASDT world with stashing'
    Add files via upload

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("Hello ASDT World with tagging")
 =======
 print("Hello ASDT World")
 >>>>>>> a48ea8b (Modify hello_world.py to display 'Hello ASDT world')
+=======
+print("Hello ASDT World with stashing")
+>>>>>>> e713cbf (Apply stashed changes for 'Hello ASDT world with stashing')
